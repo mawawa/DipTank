1a15fae32d128a1548d9bf3d099557802776e9a5
wandile <nanashongwe@gmail.com>
2025-07-06 10:20:18 +0200

    we running fast boza...

---
+++ diptank/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('about/', views.about_view, name='about'),
+    path('login/', views.login_view, name='login'),
+    path('error/', views.error_page_view, name='error_page'),
+    path('dashboard/farmer/', views.dashboard_farmer_view, name='dashboard_farmer'),
+    path('dashboard/officer/', views.dashboard_officer_view, name='dashboard_officer'),
+]
+
